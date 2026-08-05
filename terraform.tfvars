18dd44acaf99a139f74379bfdd4946c7a45c48c9
Siva <siva@Sivas-MBP.Home>
2026-08-05 12:07:12 +0100

    Use available low-cost B1s VM

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
 subscription_id = "cde0b09e-60fb-40de-85fa-f9af8b6dd549"
-vm_size         = "Standard_B1ls"
+vm_size         = "Standard_B1s"
 location        = "swedencentral"
--- variables.tf
+++ variables.tf
@@ -40,7 +40,7 @@ variable "vm_name" {
 variable "vm_size" {
   description = "Azure VM SKU."
   type        = string
-  default     = "Standard_B1ls"
+  default     = "Standard_B1s"
 }
 
 variable "admin_username" {
